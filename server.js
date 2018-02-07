e673530fb900cd95e5e324898806c5853e21dfe0
wperez185 <wperez185@yahoo.com>
2018-02-06 21:32:04 -0500

    Added new CORS headers

--- server.js
+++ server.js
@@ -27,6 +27,8 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Vary', 'Origin');
   if (req.method === 'OPTIONS') {
     return res.send(204);
   }
